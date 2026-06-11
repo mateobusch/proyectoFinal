fce2eca33b3dea567890647ef607f0500f3ee1fe
Manuelblanco0 <manuelblanco@udesa.edu.ar>
2026-06-11 17:22:04 -0300

    boton de comentarios en post y arreglando web

--- proyecto-final/src/navigation/StackSecundaria.js
+++ proyecto-final/src/navigation/StackSecundaria.js
@@ -1,6 +1,3 @@
-<Tab.Screen name="CrearPost" component={CrearPost} options={{
-                    tabBarIcon: () => <FontAwesome name="account-book" size={24} color="black"/>}}/>
-
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -13,7 +10,7 @@ export default function StackSecundaria() {
 
   return (
       <Stack.Navigator>
-        <Stack.Screen name='CrearPost' component={CrearPost} options={ {headerShown: false} }/>
+        <Stack.Screen name='NuevoPost' component={CrearPost} options={ {headerShown: false} }/>
         <Stack.Screen name='Comentarios' component={Comentarios} options={ {headerShown: false} }/>
       </Stack.Navigator>
   );
